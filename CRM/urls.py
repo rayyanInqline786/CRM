44476cb64a08054633e6fc87f7c91705ac2135df
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2021-11-26 21:40:33 +0500

    added static

--- CRM/urls.py
+++ CRM/urls.py
@@ -18,10 +18,17 @@ from django.urls import path
 from django.conf.urls import include,url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls', namespace="accounts"), name="accounts")
+    path('', include('accounts.urls', namespace="accounts"), name="accounts"),
+    
+        #for production
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
